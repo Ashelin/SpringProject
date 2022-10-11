2c69114713171dd360f5fe826541769758b6aa98
Danylo Shelest <daniel.shelest1@gmail.com>
2022-10-11 16:33:23 +0200

    Starting Project

---
+++ src/test/java/com/example/sweater/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
